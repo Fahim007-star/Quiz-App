332554d64fc6a5bfabb673bb78d9b810c3d48575
Fahim007-star <tansanfahim@gmail.com>
2026-08-22 16:33:49 +0600

    Using Type Casting

--- lib/questions_summary.dart
+++ lib/questions_summary.dart
@@ -9,7 +9,17 @@ class QuestionsSummary extends StatelessWidget {
     return Column(
       children: summaryData.map((data) {
         return Row(
-          children: [Text(((data["question_index"] as int) + 1).toString())],
+          children: [
+            Text(((data["question_index"] as int) + 1).toString()),
+            SizedBox(height: 5),
+            Column(
+              children: [
+                Text(data["question"] as String),
+                Text(data["correct_answer"] as String),
+                Text(data["user_answer"] as String),
+              ],
+            ),
+          ],
         );
       }).toList(),
     );
